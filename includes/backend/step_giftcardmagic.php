b6b639f41de602cf7d98a5a3fbd46c6c333be25c
HangCodemenschen <ntthanhhang00000@gmail.com>
2024-01-09 10:42:56 +0700

    Update

--- includes/backend/step_giftcardmagic.php
+++ includes/backend/step_giftcardmagic.php
@@ -1,47 +1,108 @@
 <div class="stepForm-giftcardMagic">
     <form action="" method="">
         <div class="chooseTemplate-giftcardMagic">
-            <h3>Choose Template</h3>
-            <ul>
-                <li><img src="" alt=""></li>
-                <li><img src="" alt=""></li>
-                <li><img src="" alt=""></li>
-                <li><img src="" alt=""></li>
-                <li><img src="" alt=""></li>
-                <li><img src="" alt=""></li>
-            </ul>
+            <h3 class="title-giftcardMagic">Choose Template</h3>
+            <div class="choose-group">
+                <label class="choose-label">
+                    <input type="radio" class="control-giftCard" name="choose-template" value="1" checked>
+                    <span class="val">
+                        <img src="" alt="">
+                    </span>
+                </label>
+                <label class="choose-label">
+                    <input type="radio" class="control-giftCard" name="choose-template" value="2">
+                    <span class="val">
+                        <img src="" alt="">
+                    </span>
+                </label>
+                <label class="choose-label">
+                    <input type="radio" class="control-giftCard" name="choose-template" value="3">
+                    <span class="val">
+                        <img src="" alt="">
+                    </span>
+                </label>
+                <label class="choose-label">
+                    <input type="radio" class="control-giftCard" name="choose-template" value="4">
+                    <span class="val">
+                        <img src="" alt="">
+                    </span>
+                </label>
+                <label class="choose-label">
+                    <input type="radio" class="control-giftCard" name="choose-template" value="5">
+                    <span class="val">
+                        <img src="" alt="">
+                    </span>
+                </label>
+                <label class="choose-label">
+                    <input type="radio" class="control-giftCard" name="choose-template" value="6">
+                    <span class="val">
+                        <img src="" alt="">
+                    </span>
+                </label>
+            </div>
         </div>
         <div class="amount-giftcardMagic">
-            <h3>Select amount</h3>
-            <ul>
-                <li>5$</li>
-                <li>10$</li>
-                <li>15$</li>
-                <li>20$</li>
-                <li>25$</li>
-                <li>30$</li>
-            </ul>
+            <h3 class="title-giftcardMagic">Select amount</h3>
+            <div class="choose-group">
+                <label class="amount-label">
+                    <input type="radio" class="control-giftCard" name="choose-amount" value="5$" checked>
+                    <span class="val">5$</span>
+                </label>
+                <label class="amount-label">
+                    <input type="radio" class="control-giftCard" name="choose-amount" value="10$">
+                    <span class="val">10$</span>
+                </label>
+                <label class="amount-label">
+                    <input type="radio" class="control-giftCard" name="choose-amount" value="15$">
+                    <span class="val">15$</span>
+                </label>
+                <label class="amount-label">
+                    <input type="radio" class="control-giftCard" name="choose-amount" value="20$">
+                    <span class="val">20$</span>
+                </label>
+                <label class="amount-label">
+                    <input type="radio" class="control-giftCard" name="choose-amount" value="25$">
+                    <span class="val">25$</span>
+                </label>
+                <label class="amount-label">
+                    <input type="radio" class="control-giftCard" name="choose-amount" value="30$">
+                    <span class="val">30$</span>
+                </label>
+            </div>
         </div>
         <div class="entryForm-giftcardMagic">
             <div style="color:#000000;" class="group-giftcardMagic">
-                <label>From: </label>
-                <input type="text" value="Calista" class="input-giftcardMagic editControl-giftcardMagic" name="your_name">
+                <label>From <span class="require">*</span> </label>
+                <div class="group-giftcardMagic">
+                    <input type="text" value="Calista" class="input-giftcardMagic editControl-giftcardMagic" name="your_name">
+                    <span class="message-error">Please enter the value</span>
+                </div>
             </div>
             <div style="color:#000000;" class="group-giftcardMagic ">
-                <label>To:</label>
-                <input type="text" value="Lillie" name="from_name" class="input-giftcardMagic editControl-giftcardMagic">
+                <label>To <span class="require">*</span></label>
+                <div class="group-giftcardMagic">
+                    <input type="text" value="Lillie" name="from_name" class="input-giftcardMagic editControl-giftcardMagic">
+                    <span class="message-error">Please enter the value</span>
+                </div>
             </div>
             <div style="color:#000000;" class="group-giftcardMagic full-width-giftcardMagic ">
-                <label>Message:</label>
-                <textarea name="message" class="input-giftcardMagic editControl-giftcardMagic">You’re the best the best the best the best the best the best</textarea>
+                <label>Message <span class="require">*</span></label>
+                <div class="group-giftcardMagic">
+                    <textarea name="message" class="input-giftcardMagic editControl-giftcardMagic">You’re the best the best the best the best the best the best</textarea>
+                    <span class="message-error">Please enter the value</span>
+                </div>
             </div>
             <div style="color:#000000;" class="group-giftcardMagic">
-                <label>Expiration:</label>
-                <input type="text" name="expiryCard" class="input-giftcardMagic editControl-giftcardMagic" value="18.08.2023">
+                <label>Expiration <span class="require">*</span></label>
+                <div class="group-giftcardMagic">
+                    <input type="text" name="expiryCard" class="input-giftcardMagic editControl-giftcardMagic" value="18.08.2023">
+                </div>
             </div>
             <div style="color:#000000;" class="group-giftcardMagic">
-                <label>Code:</label>
-                <input type="text" name="coupon_code" class="input-giftcardMagic editControl-giftcardMagic" value="AX17MA">
+                <label>Code <span class="require">*</span></label>
+                <div class="group-giftcardMagic">
+                    <input type="text" name="coupon_code" class="input-giftcardMagic editControl-giftcardMagic" value="AX17MA">
+                </div>
             </div>
         </div>
     </form>
--- includes/backend/template_1.php
+++ includes/backend/template_1.php
@@ -1,43 +1,46 @@
-<div class="item-giftcardMagic" style="background-image:url(<?php echo plugin_dir_url( __FILE__ ); ?>../../assets/images/background-demo-1.png) ;">
-    <div class="image-giftcardMagic">
-        <img src="<?php echo plugin_dir_url( __FILE__ ); ?>../../assets/images/logo.png" alt="logo company" class="logo">
-    </div>
-    <div class="content-giftcardMagic">
-        <div class="block-title-giftcardMagic">
-            <h3 class="title-giftcardMagic">GIFT CARD</h3>
-            <p class="price-giftcardMagic">
-                <span class="value-giftcardMagic">50</span>
-                <span class="currencySymbol"> $ </span></p>
-                <input type="hidden" name="voucher_value" class="input-giftcardMagic" value="50">
+<div class="wrapForm-giftcardMagic">
+    <div class="item-giftcardMagic" style="background-image:url(<?php echo plugin_dir_url( __FILE__ ); ?>../../assets/images/background-demo-1.png) ;">
+        <div class="image-giftcardMagic">
+            <img src="<?php echo plugin_dir_url( __FILE__ ); ?>../../assets/images/logo.png" alt="logo company" class="logo">
         </div>
-        <div style="color:#000000;" class="group-giftcardMagic ">
-            <label>From: </label>
-            <input type="text" readonly value="Calista" class="input-giftcardMagic" name="your_name">
-        </div>
-        <div style="color:#000000;" class="group-giftcardMagic ">
-            <label>To:</label>
-            <input type="text" readonly value="Lillie" name="from_name" class="input-giftcardMagic">
-        </div>
-        <div style="color:#000000;" class="group-giftcardMagic full-width-giftcardMagic ">
-            <label>Message:</label>
-            <textarea readonly name="message" class="input-giftcardMagic">You’re the best the best the best the best the best the best</textarea>
-        </div>
-        <div style="color:#000000;" class="group-giftcardMagic">
-            <label>Expiration:</label>
-            <input type="text" readonly name="expiryCard" class="input-giftcardMagic" value="18.08.2023">
-        </div>
-        <div style="color:#000000;" class="group-giftcardMagic">
-            <label>Code:</label>
-            <input type="text" readonly name="coupon_code" class="input-giftcardMagic" value="AX17MA">
-        </div>
-        <div class="info-giftcardMagic">
-            <div class="col-info-giftcardMagic">
-                <a href="your-site.com">your-site.com</a> | <a href="mailto:your-site@gmail.com">your-site@gmail.com</a>
+        <div class="content-giftcardMagic">
+            <div class="block-title-giftcardMagic">
+                <h3 class="title-giftcardMagic">GIFT CARD</h3>
+                <p class="price-giftcardMagic">
+                    <span class="value-giftcardMagic">50</span>
+                    <span class="currencySymbol"> $ </span></p>
+                    <input type="hidden" name="voucher_value" class="input-giftcardMagic" value="50">
             </div>
-            <div class="col-qr">
-                <img src="<?php echo plugin_dir_url( __FILE__ ); ?>../../assets/images/qr.png" alt="QR Voucher" class="qr">
+            <div style="color:#000000;" class="group-giftcardMagic ">
+                <label>From: </label>
+                <input type="text" readonly value="Calista" class="input-giftcardMagic" name="your_name">
+            </div>
+            <div style="color:#000000;" class="group-giftcardMagic ">
+                <label>To:</label>
+                <input type="text" readonly value="Lillie" name="from_name" class="input-giftcardMagic">
+            </div>
+            <div style="color:#000000;" class="group-giftcardMagic full-width-giftcardMagic ">
+                <label>Message:</label>
+                <textarea readonly name="message" class="input-giftcardMagic">You’re the best the best the best the best the best the best</textarea>
+            </div>
+            <div style="color:#000000;" class="group-giftcardMagic">
+                <label>Expiration:</label>
+                <input type="text" readonly name="expiryCard" class="input-giftcardMagic" value="18.08.2023">
+            </div>
+            <div style="color:#000000;" class="group-giftcardMagic">
+                <label>Code:</label>
+                <input type="text" readonly name="coupon_code" class="input-giftcardMagic" value="AX17MA">
+            </div>
+            <div class="info-giftcardMagic">
+                <div class="col-info-giftcardMagic">
+                    <a href="your-site.com">your-site.com</a> | <a href="mailto:your-site@gmail.com">your-site@gmail.com</a>
+                </div>
+                <div class="col-qr">
+                    <img src="<?php echo plugin_dir_url( __FILE__ ); ?>../../assets/images/qr.png" alt="QR Voucher" class="qr">
+                </div>
             </div>
         </div>
     </div>
+    <?php require_once plugin_dir_path( __FILE__ ) . 'step_giftcardmagic.php'; ?>
+
 </div>
-<?php require_once plugin_dir_path( __FILE__ ) . 'step_giftcardmagic.php'; ?>
